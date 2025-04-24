6cdb3df257b4440a2bf57598ca42b1bb0ce64297
rabbani97 <rabbanibaji.shaik97@gmail.com>
2025-04-24 03:36:25 -0500

    added secrets

--- variables.tf
+++ variables.tf
@@ -9,6 +9,26 @@ variable "location" {
     description = "The Azure region where resources will be created"
     default = "East US"
 }
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID (SPN)"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
 
 
     
